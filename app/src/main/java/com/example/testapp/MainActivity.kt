36c120415955198692d71827179cf6e9cb9a554c
Chmpragyan <pragyancham07@gmail.com>
2025-07-29 20:12:17 +0545

    Feat DIAL-1 [DONE]: Implement dialpad screen, remove material theme.

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import com.example.testapp.presentation.screen.DialPadScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
-                DialPadScreen()
-            }
+            DialPadScreen()
         }
     }
 }
